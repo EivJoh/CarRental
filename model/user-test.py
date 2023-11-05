e7ee0d3b08043dfe7fe7f9af9671c2eefa290a86
Frankarne <frankarne1@hotmail.no>
2023-11-05 18:47:36 +0100

    x

--- model/user-test.py
+++ model/user-test.py
@@ -1,79 +1,11 @@
-"""
 from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
 import re
-
-#URI = "neo4j+s://65dd3b06e30036d38da32f085934c954.neo4jsandbox.com:7687"
+"""
+URI = "neo4j+s://65dd3b06e30036d38da32f085934c954.neo4jsandbox.com:7687"
 URI = "bolt://44.197.218.79:7687"
 AUTH = ("neo4j", "candidates-collector-filters")
-
-def _get_connection() -> Driver:
-    driver = GraphDatabase.driver(URI, auth=AUTH)
-    driver.verify_connectivity()
-    return driver
 """
-
-# TEMPLATE
-from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
-import re
-
-URI = "neo4j+s://"
-AUTH = ("neo4j", "")
-
-
-def _get_connection() -> Driver:
-    driver = GraphDatabase.driver(URI, auth=AUTH)
-    driver.verify_connectivity()
-
-    return driver
-
-def findUserByUsername(username):
-    data = _get_connection().execute_query("MATCH (a:User) where a.username = $username RETURN a;", username=username)
-    if len(data[0]) > 0:
-        user = User(username, data[0][0][0]['email'])
-        return user
-    else:
-        return User(username, "Not found in DB")
-
-class Car:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def get_Username(self):
-        return self.username
-
-    def set_Username(self, value):
-        self.username = value
-
-    def get_Email(self):
-        return self.email
-
-    def set_Email(self, value):
-        self.email = value
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
-import re
-
-URI = "bolt://44.197.218.79:7687"
+URI = "neo4j+s://65dd3b06e30036d38da32f085934c954.neo4jsandbox.com:7687"
 AUTH = ("neo4j", "candidates-collector-filters")
 
 def _get_connection() -> Driver:
@@ -124,11 +56,10 @@ class Car:
     def set_Regnr(self, value):
         self.regnr = value
 
+#addCar("Mercedes", "3", "CF05101")
+#x = findCar("CF05101")
+#print(x)
 
-addCar("Mercedes", "", "AB12345")
-x = findCar("AB12345")
-print(x)
-"""
 """
 from neo4j import GraphDatabase, basic_auth
 
