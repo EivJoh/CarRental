1969d52aebed90c02cab17d7ad73834dcece02e0
Frankarne <frankarne1@hotmail.no>
2023-11-05 18:24:14 +0100

    x

--- model/user-test.py
+++ model/user-test.py
@@ -11,6 +11,65 @@ def _get_connection() -> Driver:
     driver.verify_connectivity()
     return driver
 """
+
+# TEMPLATE
+from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import re
+
+URI = "neo4j+s://"
+AUTH = ("neo4j", "")
+
+
+def _get_connection() -> Driver:
+    driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver.verify_connectivity()
+
+    return driver
+
+def findUserByUsername(username):
+    data = _get_connection().execute_query("MATCH (a:User) where a.username = $username RETURN a;", username=username)
+    if len(data[0]) > 0:
+        user = User(username, data[0][0][0]['email'])
+        return user
+    else:
+        return User(username, "Not found in DB")
+
+class Car:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def get_Username(self):
+        return self.username
+
+    def set_Username(self, value):
+        self.username = value
+
+    def get_Email(self):
+        return self.email
+
+    def set_Email(self, value):
+        self.email = value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
 import re
 
@@ -69,7 +128,7 @@ class Car:
 addCar("Mercedes", "", "AB12345")
 x = findCar("AB12345")
 print(x)
-
+"""
 """
 from neo4j import GraphDatabase, basic_auth
 
