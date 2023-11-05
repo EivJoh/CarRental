e26ed0ad091f96390e3786995f2aaa15c2afedaf
Frankarne <frankarne1@hotmail.no>
2023-11-05 12:43:59 +0100

    user-test

--- model/user-test.py
+++ model/user-test.py
@@ -1,10 +1,17 @@
-"""pip3 install neo4j-driver
 from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
 import re
 
-URI = "neo4j+s://65dd3b06e30036d38da32f085934c954.neo4jsandbox.com:7687"
-AUTH = ("neo4j", "candidates-collector-filters")"""
+#URI = "neo4j+s://65dd3b06e30036d38da32f085934c954.neo4jsandbox.com:7687"
+URI = "bolt://44.197.218.79:7687"
+AUTH = ("neo4j", "candidates-collector-filters")
 
+def _get_connection() -> Driver:
+    # driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver.verify_connectivity()
+    return driver
+
+"""
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
@@ -25,3 +32,4 @@ with driver.session(database="neo4j") as session:
     print(record['count'])
 
 driver.close()
+"""
